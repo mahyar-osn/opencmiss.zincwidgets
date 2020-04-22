b1abd546bed5288ea3b09fafa6f7b9109e53cb13
Hugh Sorby <h.sorby@auckland.ac.nz>
2020-04-22 20:59:38 +1200

    Improve long description for project.

--- setup.py
+++ setup.py
@@ -12,13 +12,28 @@ from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(here, 'src', 'opencmiss', 'utils', '__init__.py')) as fd:
+with open(os.path.join(here, 'src', 'opencmiss', 'zincwidgets', '__init__.py')) as fd:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
                         fd.read(), re.MULTILINE).group(1)
 
 if not version:
     raise RuntimeError('Cannot find version information')
 
+
+def readfile(filename, split=False):
+    with io.open(filename, encoding="utf-8") as stream:
+        if split:
+            return stream.read().split("\n")
+        return stream.read()
+
+
+readme = readfile("README.rst", split=True)
+readme.append('License')
+readme.append('=======')
+readme.append('')
+readme.append('')
+
+software_licence = readfile("LICENSE")
 
 classifiers = """\
 Development Status :: 5 - Production/Stable
@@ -37,17 +52,18 @@ Topic :: Software Development :: Libraries :: Python Modules
 
 doc_lines = __doc__.split("\n")
 
-requires = ['opencmiss.utils >= 0.2.0', 'PySide2']
+requires = ['opencmiss.utils >= 0.2.0', 'PySide2', 'opencmiss.zinc']
 
 setup(
     name='opencmiss.zincwidgets',
     version=version,
     author='H. Sorby',
     author_email='h.sorby@auckland.ac.nz',
-    packages=['opencmiss', 'opencmiss.zincwidgets'],
+    long_description='\n'.join(readme) + software_licence,
+    long_description_content_type='text/x-rst',
     platforms=['any'],
     url='https://github.com/OpenCMISS-Bindings/opencmiss.zincwidgets/',
-    license='Apache Software License',
+    license='Mozilla Public License',
     packages=find_packages("src"),
     package_dir={"": "src"},
     description=doc_lines[0],
